3dc384ef9123a9247d953d93784c9f237527164b
Kevinlike <32995731+kevinlike@users.noreply.github.com>
2022-09-01 16:25:00 +0800

    统一记录日志

---
+++ src/main/java/com/nowcoder/community/controller/aspect/AlphaAspect.java
@@ -0,0 +1,56 @@
+package com.nowcoder.community.controller.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AlphaAspect {
+    
+    /* 
+     * execution是一个固定的关键字
+     * 第一个*表示方法的返回值，任何返回值都行
+     * com.nowcoder.community.service是包名，之后的*表示所有类
+     * 之后的*表示所有方法
+     * (..)表示所有的参数
+     * 以上表示所有service下所有类，所有方法，所有参数，所有返回值都要处理
+     */
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+    @Before("pointcut()")
+    public void before(){
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after(){
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning(){
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing(){
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+        System.out.println("around Before");
+        Object obj= joinPoint.proceed();
+        System.out.println("around After");
+        return obj;
+    }
+}
---
+++ src/main/java/com/nowcoder/community/controller/aspect/ServiceLogAspect.java
@@ -0,0 +1,45 @@
+package com.nowcoder.community.controller.aspect;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.condition.RequestConditionHolder;
+
+import com.nowcoder.community.util.TimeUtil;
+
+//@Component
+//@Aspect
+public class ServiceLogAspect {
+    
+
+    private static final Logger logger=LoggerFactory.getLogger(ServiceLogAspect.class);
+
+
+    @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint){
+        //用户[1.2.3.4],在[time]，访问了[com.nowcoder.community.service.xxx()]
+
+        //获取用户ip地址
+        ServletRequestAttributes attributes= (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request=attributes.getRequest();
+        String ip=request.getRemoteHost();
+        String now=TimeUtil.date2String(new Date());
+        String target=joinPoint.getSignature().getDeclaringTypeName()+"."+joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s]，在[%s]，访问了[%s].", ip,now,target));
+    }
+}
